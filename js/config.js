9b83f60fe97aa2cdac1f25bc7ea2d060d03f6e04
gabriel C <gabcab1002@gmail.com>
2026-02-03 10:06:58 +0100

    probleme sender au lieu de from dans l'url

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const urlParams = new URLSearchParams(window.location.search);
 
 export const playerName = urlParams.get('name') || "toi";
 export const gender = urlParams.get('gender') || "x";
-export const from = urlParams.get('sender') || "ton admirateur secret";
+export const from = urlParams.get('from') || "ton admirateur secret";
 
 export let crushWord = "mon amour";
 if (gender === "m") crushWord = "mon valentin";
